c0c0d9fb8b6ef0f4bd2ffeaa3cc100622d3f51b4
vasiljevdenis <vasiljevdenis@bk.ru>
2022-03-21 13:37:36 +0300

    v 1.0

---
+++ js/script.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+    let nextButtons = document.querySelectorAll('.btn-success');
+    let steps = document.querySelectorAll('.step');
+    let mainHeight = document.querySelector('#main').offsetHeight;
+    let accordion = document.querySelector('.accordion-collapse');
+    const collapse = new bootstrap.Collapse(accordion, {toggle: false});
+    let i = 0;
+
+    for (let i = 0; i < steps.length; i++) {
+        const element = steps[i];
+        element.style.height = mainHeight + "px";
+    }
+
+    for (let i = 0; i < nextButtons.length; i++) {
+        const button = nextButtons[i];
+        button.addEventListener('click', scrollToId);
+    }
+
+    function scrollToId() {
+        if(accordion.classList.contains('show')) collapse.toggle();
+        steps[i].classList.remove('off');
+        steps[i].classList.add('on');
+        let target = steps[i];
+
+        if (target !== null) {
+            let pos = target.offsetTop;
+
+            window.scrollTo({
+                top: pos,
+                behavior: "smooth"
+            });
+        }
+        nextButtons[i].setAttribute('disabled', 'disabled');
+        i++;
+    }
+
+}
